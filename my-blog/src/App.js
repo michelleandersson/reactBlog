dd1584e31a22c01629383f6e316e7fd41118081d
Michelle Andersson <michelleandersson@live.com>
2020-11-17 09:50:05 +0100

    add articles and navbar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/homePage';
 import AboutPage from './pages/AboutPage'; 
 import ArticleList from './pages/ArticleList'; 
 import ArticlePage from './pages/ArticlePage'; 
+import NavBar from './navBar'; 
 import './App.css';
 
 
@@ -14,11 +15,12 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component ={HomePage} exact />
           <Route path="/about" component ={AboutPage} />
           <Route path="/articles-list" component ={ArticleList} />
-          <Route path="/article" component ={ArticlePage} />
+          <Route path="/article/:name" component ={ArticlePage} />
         </div>
       </div>
     </Router>
