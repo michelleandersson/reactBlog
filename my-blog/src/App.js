1111ef7de449749117a3d83dfe2ea8dcd1716e9e
Michelle Andersson <michelleandersson@live.com>
2020-11-17 13:31:29 +0100

    add error 404 page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -9,6 +10,7 @@ import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './navBar';
 import './App.css';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   render() {
@@ -17,10 +19,13 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <div id="page-body">
-            <Route path="/" component={HomePage} exact />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesListPage} />
-            <Route path="/article/:name" component={ArticlePage} />
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
           </div>
         </div>
       </Router>
